ff301746c571a3cab5a988232ef2e49d3ab1f0a7
M-Montecino <eduardamontecino@gmail.com>
2025-09-17 16:20:50 -0300

    atualizacao1

---
+++ trabalhinho_lista.py
@@ -0,0 +1,71 @@
+class Elemento:
+    def __init__(self, dado):
+        self.dado = dado
+        self.__proximo = None
+        self.__anterior = None
+
+    @property
+    def proximo(self) -> "Elemento":
+        return self.__proximo
+    
+    @proximo.setter
+    def proximo(self, proximo:"Elemento"):
+        if isinstance(proximo, Elemento):
+            self.__proximo = proximo
+    
+    @property
+    def anterior(self) -> "Elemento":
+        return self.__anterior
+    
+    @anterior.setter
+    def anterior(self, anterior:"Elemento"):
+        if isinstance(anterior, Elemento):
+            self.__anterior = anterior
+
+class ListaDuplamenteEncadeada:
+    def __init__(self):
+        self.__primeiro = None
+        self.__ultimo = None
+        self.__tamanho = 0
+        self.__cursor = None
+
+    def __avancar_k_posicoes(self, k:int):
+        for i in range(k):
+            if self.__cursor and self.__cursor.proximo:
+                self.__cursor = self.__cursor.proximo
+            else:
+                break
+    
+    def __retroceder_k_posicoes(self, k:int):
+        for i in range(k):
+            if self.__cursor and self.__cursor.anterior:
+                self.__cursor = self.__cursor.anterior
+            else:
+                break
+
+    def __ir_para_o_primeiro(self):
+        self.__cursor = self.__primeiro
+
+    def __ir_para_o_ultimo(self):
+        self.__cursor = self.__ultimo
+
+    def acessar_atual(self):
+        return self.__cursor
+    
+    def inserir_antes_do_atual(self, novo:Elemento):
+        if self.__cursor is None:
+            self.__primeiro = self.__ultimo = self.__cursor = novo
+        else:
+            
+    
+    def inserir_apos_atual():
+
+    def inserir_como_primeiro():
+
+    def inserir_como_ultimo():
+    
+    def excluir_atual():
+    
+    def excluir_primeiro():
+    
+    def excluir_ultimo():
